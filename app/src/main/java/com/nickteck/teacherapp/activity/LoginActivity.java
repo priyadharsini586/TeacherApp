48c4f26373eb45ef984c8e2d0ce1b9fa00a9afd3
santhiya <sandhiya1606@gmail.com>
2018-06-29 20:00:31 +0530

    login and dashboard designs

--- app/src/main/java/com/nickteck/teacherapp/activity/LoginActivity.java
+++ app/src/main/java/com/nickteck/teacherapp/activity/LoginActivity.java
@@ -1,15 +1,46 @@
 package com.nickteck.teacherapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.nickteck.teacherapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText editPhoneNo;
+    Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login_screen);
+
+        init();
+        setOnclickListener();
+    }
+
+    private void init() {
+        editPhoneNo = (EditText) findViewById(R.id.editPhoneNo);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+    }
+
+    private void setOnclickListener() {
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  =new Intent(LoginActivity.this,DashBoardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nickteck/teacherapp/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.nickteck.teacherapp.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.nickteck.teacherapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+    int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
